73b517a1bdaae84cd152c7828b8d659f18193e5a
KVYU <45233096+wangdizzy@user.noreply.github.com>
2024-08-07 11:52:01 +0800

    2024/08/07 from_json_keyfile_name change from_json_keyfile_dict

--- ManualCheckUrl.py
+++ ManualCheckUrl.py
@@ -14,16 +14,34 @@ import gspread
 """
 檢查sta sta2
 """
-print('-----start-----')
+print("-----start-----")
 message = ""
+sheet_json = {
+    "type": "service_account",
+    "project_id": "inlaid-sentinel-407402",
+    "private_key_id": "e8289caa403c29ec8edeb62490cdb696984dea98",
+    "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQC0FOWiThPW8PVC\nHxJT6zFuXVE5ekvsIDiDzznJRorYkZFRxV6n6H2c6rBUZQf2dhgYR6HorFcMz+oy\n9ihlO2Fv7fXD6bfn7xje56JPyVu3RXsmAB1K7eZYFjWYeULYe3Xl5eLZX4QYFSvI\nBK/CX+pboojywnPcJlSBxqW1q9cz+crvpfTXq5mPEdp1sdotOxmX6pTi8SvB4eSd\n3A31oCtpsEvXFwK1ZUwEVlw7cLdgGqhKrEgn8wHVrYiVYSA7pM19opy2gJJyqZVX\n6wzeGA1uBGTXDAEMBLJ8AStuFAjoG9/3OMay0Ln3ba9lc7vOWsdBFz/TqdSaRGre\nUJuDuFvJAgMBAAECggEAEfQOY8TIdhKeRPoPi/XD/xaHGUWfyZn5wGxZvL8PayFy\nHTah5fgIA+ui6jsLVO83nj4P/oAmCpU06mE/rD4EDBJrgN1tdA5SirCJrk4rGmWv\nLh3vTa/Tmd8W901JlIcUfTfSyqya4QLFU2LiOezxkrKs2BT6U42vuaN6FFdeNGRt\nPLPxSnNHwpv59XrCfFoOp78oU3LrE+J7DQrjITBSy3iVZQypW+x5JapJ9R5Ix5Ox\njHehNaqTJ4K5y31ZBEur2ZQTNEd95i+gBIHAJbb3L49C2zt0TwFbEVkdOeG5zZ3l\nJr9oPRYQN5QwlgZgIW8T5SKlRA8rsv7wAwm9qV/XXQKBgQDoVrQiTafWuLD1VHxJ\n3uMsI+dzMFRBKGmd8tbFD5V1atFrnXSTVBtg2RdsAM21WIG8D0EhLD5Kc7XlpIPv\n9sMWEaKKbGLk8a4eKXAEetsJjnnCdCLb+GqeO2Qp0JDZ6cKwZFKNopcDEEiEX39i\nZtHL82Eq7DbnkLJ0G8dwqZJB7QKBgQDGa80FkScVnyw5G0PEqtQ1wAfytJyGB3V6\nJDNdaC8XXm2vzUHWZM/Tyrw+qeHZRuLDu2gb2p221zE7CyYkxgj66VWBNRPnpEiY\nc3VV7bXBfVEAiftVRWAHgrHtGCRACKKt4OgfrJrzRAcekye4Lm4er4QDEZI4uBZq\nYm8fqVG1zQKBgQDegDoeJ9Q2M8V0DKbCb6uK2A+NJplplPQgiTDYo2X0folzz+SW\nOxPFGeHuUo6tvsbvfIRY6m/1CP8HnxejNOP7PIQ2oDnNGw4uYGygPa+KZWGBsYq4\nshwY0LPJv60Yo18JYeoVLcIE8xEfg/0QFXuRH9DMNE8YUGA2BWxoHlysuQKBgQCl\nRMcpGuTeGo1gJ3iDx/InrwIvwwYYkP/ls19hLtUCdvGPm7x50dBVTSkMXL20F1nr\nxB4MDUSONaFY14l22chDDbTdgRNKPskEyi5yWyOnvTSJ6WQBe15oAxEmNZSEDW1K\nvOk68K7DbucrLVDJFUs9nd2sHKeZPKPXCpQaYBKiBQKBgEdAQVOL6p5sm/acvKsF\nSzzieDOzCeeiGYmeqguiEg7sLJ5UcSlCdKDGKFtgc1vi9j1cVcAah2PBgigyuLhx\nrkHfPNKePKZ/Do3WTuG2HOmlRXK/FJXsE+9m6joD9ZVipE2mTowWNq675EkUkfrd\n/Ef2Q0gOIPY2sY5/aKQKR18P\n-----END PRIVATE KEY-----\n",
+    "client_email": "qa-451@inlaid-sentinel-407402.iam.gserviceaccount.com",
+    "client_id": "115465534078375635707",
+    "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+    "token_uri": "https://oauth2.googleapis.com/token",
+    "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+    "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/qa-451%40inlaid-sentinel-407402.iam.gserviceaccount.com",
+    "universe_domain": "googleapis.com",
+}
+
 
 scopes = [
     "https://spreadsheets.google.com/feeds"
 ]  # 定義存取的範圍 feeds = google sheet
 
-credentials = ServiceAccountCredentials.from_json_keyfile_name(
-    "sheet.json", scopes
-)  # 指定檔案金鑰
+# 使用json檔案金鑰
+# credentials = ServiceAccountCredentials.from_json_keyfile_name(
+#    "sheet.json", scopes
+# )
+
+# 值將將金鑰放在di使用
+credentials = ServiceAccountCredentials.from_json_keyfile_dict(sheet_json, scopes)
 
 client = gspread.authorize(credentials)  # 傳入gspread模組
 
@@ -97,7 +115,10 @@ options.add_argument(
 options.add_experimental_option("excludeSwitches", ["enable-logging"])
 options.add_argument("--start-maximized")  # 視窗最大
 chrome = webdriver.Chrome(
-    service=ChromeService(ChromeDriverManager(driver_version="126.0.6478.183").install()), options=options
+    service=ChromeService(
+        ChromeDriverManager(driver_version="126.0.6478.183").install()
+    ),
+    options=options,
 )
 
 # 用request打URL 確認status code
@@ -117,17 +138,23 @@ for x in url:
                 chrome.find_element(By.ID, "btn-login").click()
                 chrome.implicitly_wait(5)  # 隱含等待
                 # 判斷是否要更新密碼
-                oldPwd = chrome.find_element(By.XPATH, '//*[@id="view-home"]/div[2]/div/div/div[1]').text
-                if oldPwd != '':
+                oldPwd = chrome.find_element(
+                    By.XPATH, '//*[@id="view-home"]/div[2]/div/div/div[1]'
+                ).text
+                if oldPwd != "":
                     message += "密碼需要更新%s\n" % x
                 else:
                     chrome.implicitly_wait(5)  # 隱含等待
                     chrome.find_element(
-                        By.XPATH, '//*[@id="link-sport"]/div[1]/img').click()
+                        By.XPATH, '//*[@id="link-sport"]/div[1]/img'
+                    ).click()
                     chrome.find_element(
-                        By.XPATH, '//*[@id="toolbar-sport"]/div/a[4]/i').click()
+                        By.XPATH, '//*[@id="toolbar-sport"]/div/a[4]/i'
+                    ).click()
                     chrome.find_element(
-                        By.XPATH,"/html/body/div[7]/div[1]/div/ul/li[3]/a/div[2]/div[1]",).click()
+                        By.XPATH,
+                        "/html/body/div[7]/div[1]/div/ul/li[3]/a/div[2]/div[1]",
+                    ).click()
                     print("%s 可以正常切換Market Type(新手機)" % x)
                     continue
             except:
@@ -189,14 +216,19 @@ for x in url:
                             else:
                                 message += "無法取得BetView %s\n" % x
                         except:
-                            message += "無法取得BetView %s\n" % x 
+                            message += "無法取得BetView %s\n" % x
                     try:
                         chrome.implicitly_wait(5)  # 隱含等待
                         chrome.switch_to.frame("leftFrame")  # 切換frame
                         chrome.find_element(By.ID, "btn_staus").click()
-                        balance = chrome.find_element(By.ID, "lb_balance").text.replace(",", "")  # 取得Balance金額
+                        balance = chrome.find_element(By.ID, "lb_balance").text.replace(
+                            ",", ""
+                        )  # 取得Balance金額
                         outstanding = chrome.find_element(
-                            By.ID, "lb_outstanding").text.replace(",", "")  # 取得Outstanding金額
+                            By.ID, "lb_outstanding"
+                        ).text.replace(
+                            ",", ""
+                        )  # 取得Outstanding金額
                         print("正常取得用戶金額 %s" % x)
                     except:
                         message += "無法取得用戶金額 %s\n" % x
